600eb2a9d168b6bc3d9d983b08aa9329d0c24d85
salmeleh <salmeleh@gmail.com>
2016-03-08 09:40:34 -0600

    master-detail demo

---
+++ Master-Detail/Master-DetailTests/Master_DetailTests.swift
@@ -0,0 +1,36 @@
+//
+//  Master_DetailTests.swift
+//  Master-DetailTests
+//
+//  Created by Stu Almeleh on 3/8/16.
+//  Copyright © 2016 Stu Almeleh. All rights reserved.
+//
+
+import XCTest
+@testable import Master_Detail
+
+class Master_DetailTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
