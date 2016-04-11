a5011c1ab21cb121fcec7a728fb51a03147f07d3
salmeleh <salmeleh@gmail.com>
2016-04-11 09:01:13 -0500

    mapView working

---
+++ VirtualTourist/VirtualTourist/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Stu Almeleh on 4/11/16.
+//  Copyright © 2016 Stu Almeleh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var loadingWheel: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadingWheel.hidesWhenStopped = true
+        // set initial location in Chicago
+        let initialLocation = CLLocation(latitude: 41.881832, longitude: -87.623177)
+        centerMapOnLocation(initialLocation)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    
+    
+    
+    //radius helper
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+}
+
